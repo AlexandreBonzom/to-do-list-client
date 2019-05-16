cef8193046f09b899030617024668ee31165d244
Alexandre Bonzom <bonzom.alexandre@gmail.com>
2019-05-16 17:50:11 +0200

    modification from React classes to React with hooks

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { Component } from "react";
+import React, { useState, useEffect } from "react";
 import axios from "axios";
 import "./App.css";
 import SearchBar from "./component/SearchBar";
@@ -7,94 +7,91 @@ import Header from "./component/Header";
 import List from "./component/List";
 import Button from "./component/Button";
 
-class App extends Component {
-  state = {
-    todos: [],
-    newTask: "",
-    search: "",
-    onlyRemainingTasks: false
-  };
-
-  componentDidMount = async () => {
-    this.updateList();
-  };
-
-  updateList = () => {
+function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect(() => {
     axios
       .get("https://to-do-list-server-exercice.herokuapp.com/")
       .then(response => response.data.tasks, error => console.log(error))
-      .then(todos => this.setState({ todos: todos }));
+      .then(todos => setTodos(todos));
+  }, []);
+
+  const [newTask, setNewTask] = useState("");
+  const [search, setSearch] = useState("");
+  const [onlyRemainingTasks, setFilter] = useState(false);
+
+  const updateList = () => {
+    axios
+      .get("https://to-do-list-server-exercice.herokuapp.com/")
+      .then(response => response.data.tasks, error => console.log(error))
+      .then(todos => setTodos(todos));
   };
 
-  handleInputChange = event => {
-    this.setState({ [event.target.name]: event.target.value });
+  const handleInputChange = event => {
+    if (event.target.name === "newTask") {
+      setNewTask(event.target.value);
+    } else if (event.target.name === "search") {
+      setSearch(event.target.value);
+    }
   };
 
-  handleAddClick = async () => {
-    const { newTask } = this.state;
-    const todos = [...this.state.todos].map(todo => todo.task.toLowerCase());
+  const handleAddClick = async () => {
+    const newTodos = [...todos].map(todo => todo.task.toLowerCase());
 
-    if (!todos.filter(task => task === newTask.toLowerCase()).length) {
+    if (!newTodos.filter(task => task === newTask.toLowerCase()).length) {
       axios
         .post("https://to-do-list-server-exercice.herokuapp.com/create", {
           task: newTask
         })
-        .then(() => this.updateList(), error => console.log(error))
-        .then(() => this.setState({ newTask: "" }));
+        .then(() => updateList(), error => console.log(error))
+        .then(() => setNewTask(""));
     }
   };
 
-  handleClickOnElement = task => {
+  const handleClickOnElement = task => {
     axios
       .post("https://to-do-list-server-exercice.herokuapp.com/update", {
         id: task._id,
         isDone: !task.isDone
       })
-      .then(() => this.updateList(), error => console.log(error));
+      .then(() => updateList(), error => console.log(error));
   };
 
-  handleDeleteClick = task => {
+  const handleDeleteClick = task => {
     axios
       .post("https://to-do-list-server-exercice.herokuapp.com/delete", {
         id: task._id
       })
-      .then(() => this.updateList(), error => console.log(error));
+      .then(() => updateList(), error => console.log(error));
   };
 
-  handleClickSeeOnlyRemainingTask = () => {
-    const { onlyRemainingTasks } = this.state;
-    this.setState({ onlyRemainingTasks: !onlyRemainingTasks });
+  const handleClickSeeOnlyRemainingTask = () => {
+    setFilter(!onlyRemainingTasks);
   };
 
-  render() {
-    return (
-      <div className="page">
-        <div className="container">
-          <Header />
-          <SearchBar
-            handleInputChange={this.handleInputChange}
-            value={this.state.search}
-          />
+  return (
+    <div className="page">
+      <div className="container">
+        <Header />
+        <SearchBar handleInputChange={handleInputChange} value={search} />
 
-          <List
-            {...this.state}
-            toggleTask={this.handleClickOnElement}
-            clickDelete={this.handleDeleteClick}
-            clickHide={this.handleClickSeeOnlyRemainingTask}
-          />
-          <NewTask
-            handleInputChange={this.handleInputChange}
-            value={this.state.newTask}
-          />
-          <Button
-            addTodo={this.handleAddClick}
-            label="ajoute une tâche"
-            name="add-task"
-          />
-        </div>
+        <List
+          todos={todos}
+          onlyRemainingTasks={onlyRemainingTasks}
+          search={search}
+          toggleTask={handleClickOnElement}
+          clickDelete={handleDeleteClick}
+          clickHide={handleClickSeeOnlyRemainingTask}
+        />
+        <NewTask handleInputChange={handleInputChange} value={newTask} />
+        <Button
+          addTodo={handleAddClick}
+          label="ajoute une tâche"
+          name="add-task"
+        />
       </div>
-    );
-  }
+    </div>
+  );
 }
 
 export default App;
